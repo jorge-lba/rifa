f41142ab2a45ad8062990164663edfcd3352b96f
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-02-29 01:24:26 -0300

    Add funçao para Adicionar novo comprador

--- functions/src/database/index.js
+++ functions/src/database/index.js
@@ -1,5 +1,39 @@
 const lowdb = require( 'lowdb' )
-const FileAsync = require( 'lowdb/adapters/FileAsync' )
+const FileSync = require( 'lowdb/adapters/FileSync' )
 
-const adapter = new FileAsync( __dirname + '/database.json' )
+const adapter = new FileSync( __dirname + '/database.json' )
 const database = lowdb( adapter )
+
+database.defaults({
+    buyers: [],
+    requests: []
+}).write()
+
+const dbSetBuyers = ( optionsObject ) => {
+    if( !Object.keys( optionsObject )[0] ) return { error: 'Object Undefined' }
+
+    try {
+        if( !optionsObject.id )
+            optionsObject.id = database.get( 'buyers' ).value().length
+    
+        if( !optionsObject.date )
+            optionsObject.date = new Date
+    
+        const { id, email, name, numbers, date } =  optionsObject
+    
+        database.get( 'buyers' ).push( {
+            id, email, name, numbers, date
+        } ).write()  
+        
+        return { msg: 'Buyer successfully added' }
+    } catch (error) {
+        console.log( error )
+        return { error }
+    }
+}
+
+console.log(dbSetBuyers( {
+    email: 'jorgeshawee@gmail.com',
+    name: 'Jorge Alegretti',
+    numbers: [ 12, 45, 423]
+} ))
