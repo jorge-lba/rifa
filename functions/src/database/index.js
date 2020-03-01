d438c5648433be16369cbeba8c4d62f3e20e5cfc
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-02-29 21:03:09 -0300

    Add funçao para update do cadastro

--- functions/src/database/index.js
+++ functions/src/database/index.js
@@ -9,6 +9,15 @@ db.defaults({
     requests: []
 }).write()
 
+const testNumbers = ( current, next ) => {
+    const result = []
+    next.forEach( ( number, index ) => {
+      if( current.indexOf( number ) === -1 ) result.push( number )
+    } )
+    const res = [ ...current, ...result ]
+    return res
+  }
+
 const dbSetBuyers = ( optionsObject, database = db ) => {
     if( !Object.keys( optionsObject )[0] ) return { error: 'Object Undefined' }
 
@@ -37,7 +46,19 @@ const dbSetBuyers = ( optionsObject, database = db ) => {
 const dbGetBuyers = ( database = db ) => database.get( 'buyers' ).value()
 const dbFilterBuyer = ( object, database = db ) => database.get( 'buyers' ).filter( object ).value() 
 
-const dbUpadateBuyers = ( optionsObject ) => {
+const dbUpdateBuyers = ( buyerData, update, database = db ) => {
+    const buyer = database.get( 'buyers' ).find( buyerData )
+    const [ ...updateElement ] = Object.keys( update )
+
+    updateElement.forEach( element => {
+        console.log( update[ element ] )
+        if( element === 'id' ) return
+        element === 'numbers'
+            ? buyer.update( 'numbers', res => testNumbers( res, update[ element ] ) ).write()
+            : buyer.update( element, res => update[ element ] || res).write()
+    } )
+
+    return "Update OK"
 
 }
 
@@ -48,8 +69,17 @@ const dbUpadateBuyers = ( optionsObject ) => {
 // } ))
 
 // console.log( dbGetBuyers( ) )
-console.log( dbFilterBuyer( {
+// console.log( dbFilterBuyer( {
+//     id: 2,
+//     email: 'jorgeshawee@gmail.com',
+//     name: 'Jorge Alegretti'
+// }) )
+
+console.log( dbUpdateBuyers( {
     id: 2,
+}, {
+    id: 10,
     email: 'jorgeshawee@gmail.com',
-    name: 'Jorge Alegretti'
+    name: 'Alegretti',
+    numbers: [ 12,25,58,70 ]
 }) )
