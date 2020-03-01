dcbf81247bde63c28c8511da67db82c625444942
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-03-01 09:41:30 -0300

    Add express

--- functions/index.js
+++ functions/index.js
@@ -1,41 +1,42 @@
 const functions = require('firebase-functions');
-// const admin = require("firebase-admin");
+const express = require( 'express' )
+const cors = require( 'cors' )
+const bodyParser = require( 'body-parser' )
 
-// // Fetch the service account key JSON file contents
-// const serviceAccount = require("./.user/rifa-99freelas-firebase-adminsdk-gmdt2-c75b1924e0.json");
-
-// // Initialize the app with a service account, granting admin privileges
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   databaseURL: "https://rifa-99freelas.firebaseio.com"
-// });
-
-// // As an admin, the app has access to read and write all data, regardless of Security Rules
-// const db = admin.database();
-// const ref = db.ref("restricted_access/secret_document");
-// ref.once("value", function(snapshot) {
-//   console.log(snapshot.val());
-// });
-
-// const buyersRef = ref.child( 'buyers/1' )
+const app = express()
 
 const { dbSetBuyers, dbGetBuyers, dbFilterBuyer, dbUpdateBuyers, dbRemoveAllBuyers } = require( './src/database/index' )
+
+app.use( cors() )
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+exports.app = functions.https.onRequest( app ) 
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 
-exports.setBuyer = functions.https.onRequest((request, response) => {
-  dbSetBuyers( {
-    // id: 3, 
-    email: 'alegretti@test.com',
-    name: 'alegretti',
-    numbers: [20,98,65]
-  } )
- response.send("Add");
-});
+// exports.setBuyer = functions.https.onRequest((request, response) => {
+//   dbSetBuyers( {
+//     // id: 3, 
+//     email: 'alegretti@test.com',
+//     name: 'alegretti',
+//     numbers: [20,98,65]
+//   } )
+//  response.send("Add");
+// });
 
-exports.deleteBuyer = functions.https.onRequest((request, response) => {
-  dbRemoveAllBuyers()
- response.send("Delete");
-});
+// exports.getAllBuyers = functions.https.onRequest( async ( request, response )=> {
+//   const res = await dbGetBuyers()
+//   console.log( res )
+//   response.send( res )
+// } )
+
+// exports.deleteBuyer = functions.https.onRequest((request, response) => {
+//   dbRemoveAllBuyers()
+//  response.send("Delete");
+// });
+
+
 
