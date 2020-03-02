6ce77600290e15242b4204248ff2965c0364b584
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-03-02 20:19:00 -0300

    Add Funçao para selecionar o numero

--- public/src/home/index.js
+++ public/src/home/index.js
@@ -1,9 +1,13 @@
-const numbersSection = document.querySelector( '#numbers' )
+const $ = ( element ) => document.querySelector( element )
+
+const numbersSection = $( '#numbers' )
 const numbersEnd = 100
 
 const numbersHTML = ( number ) => {
     const node = document.createElement( `div` )
-    node.setAttribute( 'id', `${ number }` )
+    node.setAttribute( 'id', `num_${ number }` )
+    node.setAttribute( 'class', `open` )
+    
     node.innerHTML = 
         `<input type="checkbox" class="numbers-checkbox" id="${ number }">
         <label for="number">${ number }</label>`
@@ -17,5 +21,31 @@ const createNumbers = ( total = numbersEnd, element = numbersSection ) => {
         element.appendChild( numbersHTML( i ) )
     }
 }
-
 createNumbers( )
+
+const addClass = ( className, id ) => {
+    const element = $( `#num_${ id }` )
+    element.setAttribute( 'class', className )
+}
+
+const getClassName = ( element ) => $( `#num_${ element }` ).className
+
+numbersSection.addEventListener('mousedown', ( element ) => {
+    const tag = element.toElement.localName
+    const text = element.toElement.innerText
+    if( tag === 'div' || tag === 'label' ){
+
+        switch( getClassName( text ) ){
+            case 'selected':
+                addClass( 'open', text )
+            break
+            case 'open':
+            case 'pending':
+                addClass( 'selected', text )
+            break
+
+        }
+
+
+    }
+} )
