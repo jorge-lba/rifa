51697077566e97c6ed9f6f5d301d8bff3eaf39b8
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-03-04 17:52:43 -0300

    Funcionalidade de envio de email funcionando - nodemailer

--- functions/src/model/sendEmail.js
+++ functions/src/model/sendEmail.js
@@ -1,14 +1,15 @@
-const sgMail = require("@sendgrid/mail")
+const nodemailer = require('nodemailer');
+const smtpTransport = require('nodemailer-smtp-transport');
 const cors = require( 'cors' )( { origin: true } )
 const { configTransporter, mailOptions, url, sendgridKEY } = require( './.configEmail.js' )
 
-sgMail.setApiKey( sendgridKEY )
+const  transporter = nodemailer.createTransport(smtpTransport( configTransporter ))
 const send = ( options = mailOptions ) => {
-    try {
-        sgMail.send(options) 
-    } catch (error) {
-        console.log( error )
-    }
+    transporter.sendMail( options, function(error, info){
+        if(error){
+           console.log(error.message);
+        }   
+    })
 }
 
 const emailS = { send }
