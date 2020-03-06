1babbbb130ef8c629f6d2cb103134e5aaf46c21a
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-03-06 16:05:12 -0300

    Add cheerio no projeto

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,7 @@ const cors = require( 'cors' )
 const bodyParser = require( 'body-parser' )
 const { emailS, mailOptions } = require( __dirname + '/src/model/sendEmail.js' )
 const htmlBase = require( __dirname + '/src/view/emailConfirm.html' )
+const cheerio = require( 'cheerio' )
 
 
 const app = express()
